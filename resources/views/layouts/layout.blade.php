f7cbd0eebc8060e7f72f0d5b3eca0072e70322aa
Sylvester Amaechi <chinonso1amaechi@gmail.com>
2019-01-29 02:14:42 +0100

    first commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,7 @@
+@include('layouts.header')
+
+@include('layouts.nav')
+
+@yield('content')
+
+@include('layouts.footer')
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () { return view('index');});
+
+Route::get('/store', function () {return view('store');});
+
+Route::get('/product', function () {return view('product');});
+
+Route::get('/checkout', function () {return view('checkout');});
+
+Route::get('/blank', function () { return view('blank');});
+
