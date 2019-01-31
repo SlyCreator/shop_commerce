451fe9c6876c9aaf10d26175c4b33b980a2d471f
Sylvester Amaechi <chinonso1amaechi@gmail.com>
2019-01-31 21:54:38 +0100

    update indexcontroller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,7 +8,28 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index(){
-        $products = Product::inRandomOrder()->take(8)->get();
-        return view('index',['products'=>$products]);
+        $rand_products = Product::inRandomOrder()->take(8)->get();
+       // $products_desc =$this->productsDesc();
+       // $products_asc  =$this->productsAsc();
+
+        $products_desc =Product::orderBy('p_name','desc')->take(3)->get();
+        $products_asc  =Product::orderBy('p_name','asc')->take(3)->get();
+        $col1_products = Product::inRandomOrder()->take(3)->get();
+        $col2_products = Product::inRandomOrder()->take(3)->get();
+        $col3_products = Product::inRandomOrder()->take(3)->get();
+
+      
+        return view('index')->with(compact('rand_products','products_desc','products_asc','col1_products','col2_products','col3_products'));
+        //return view('index',['products'=>$products]);
+    }
+
+    public function productsDesc(){
+        $products = Product::orderBy('p_name','desc')->get();
+        return $products;
+    }
+
+    public function productsAsc(){
+        $products = Product::orderBy('p_name','asc')->get();
+        return $products;
     }
 }
