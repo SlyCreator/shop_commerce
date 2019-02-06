c99bf9f258396a1bd6cbd61dec085882ed1637c9
Sylvester Amaechi <chinonso1amaechi@gmail.com>
2019-02-06 22:52:51 +0100

    figured out the shipping and sessions

--- database/migrations/2019_02_01_091027_create_sessions_table.php
+++ database/migrations/2019_02_01_091027_create_sessions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrdersTable extends Migration
+class CreateSessionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('sessions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('visitor');
+            $table->string('session');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('sessions');
     }
 }
---
+++ database/migrations/2019_02_02_144604_create_shippings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShippingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shippings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('f_name');
+            $table->string('l_name');
+            $table->string('email');
+            $table->string('address');
+            $table->string('state');
+            $table->string('country');
+            $table->integer('zipcode');
+            $table->integer('telephone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shippings');
+    }
+}
