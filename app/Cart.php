2a0604f71044a2f1dc9a3077a7b65fb1748f8ff8
Sylvester Amaechi <chinonso1amaechi@gmail.com>
2019-02-11 20:32:48 +0100

    update on cart  feb 11

--- app/Cart.php
+++ app/Cart.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    //
+    protected $table ='carts';
+    protected $primaryKey='id';
+    protected $fillable = [
+        '_token' =>'session_id',
+        'product_id',
+        'quantity',
+        'size',
+        'color'
+    ] ;
 }
--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
@@ -17,25 +18,25 @@ class CartController extends Controller
             $Insert_product = request()->all();
 
                 //check if item is still in stock
-                 if(1==1){
-
-                    //get the session id or the token pass
-                    $session_id = Session::get('session_id');
-                    if(empty($session_id)){
-                        $session_id = $Insert_product['_token'];
-                        //check if token was pass
+                 if(1==1){             
+                    $session_id = Session::get('session_id');       
+                        //check if token is passed if not set it
                         if(empty($session_id)){
                             $session_id =str_random(40);
+                            Session::put('session_id', $session_id);
+                        }           
+                    $Insert_product['session_id'] = $session_id;
+            
+                    //check to see if item and it spec already exist to avoid duplicate
+                    $check_duplicate = Cart::where(['product_id'=>$Insert_product['p_id'],
+                                                    'color'     =>$Insert_product['p_color'],
+                                                    'size'      =>$Insert_product['p_size']])->count();
+                        if($check_duplicate>0){
+                            echo "Item added b4";
                         }
-                        Session::put('session_id',$session_id);
-                    }
-                   $Insert_product['session_id'] = $session_id;
-                   $Insert_product['size']= $size;
-                   //check to see if item and it spec already exist to avoid duplicate
-                   $check_duplicate = Cart::where(['']);
-
-
-                    //return back()->with('message','Added to cart');
+                        Cart::create($Insert_product);
+                            echo"thank you holyspirit";
+                        //return back()->with('message','Added to cart');
                 }else{
                     return back()->with('message','Stock is not Available');
                 }
